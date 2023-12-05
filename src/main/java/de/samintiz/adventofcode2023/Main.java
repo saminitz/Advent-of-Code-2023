59203c21a9d367f80635c60556474b2413785348
Samuel Nitzsche <samuel.nitzsche+github@gmail.com>
2023-12-05 18:09:13 +0100

    added elapsed time to main function

--- src/main/java/de/samintiz/adventofcode2023/Main.java
+++ src/main/java/de/samintiz/adventofcode2023/Main.java
@@ -1,5 +1,7 @@
 package de.samintiz.adventofcode2023;
 
+import java.util.concurrent.TimeUnit;
+
 import de.samintiz.adventofcode2023.day.DayManager;
 import de.samintiz.adventofcode2023.ui.Ui;
 
@@ -11,13 +13,29 @@ public class Main {
     }
 
     private static void executeBothParts(int dayNumber) {
+        long startTime = System.currentTimeMillis();
+
+        String partOne = DayManager.getDayInstance(dayNumber).partOne();
+        String partTwo = DayManager.getDayInstance(dayNumber).partTwo();
+
+        long endTime = System.currentTimeMillis();
+        long elapsedTime = endTime - startTime;
+
+        long minutes = TimeUnit.MILLISECONDS.toMinutes(elapsedTime);
+        long seconds = TimeUnit.MILLISECONDS.toSeconds(elapsedTime);
+        long millis = TimeUnit.MILLISECONDS.toMillis(elapsedTime);
+
         System.out.println(String.format("""
 
                 Day %d:
                     Part 1: %s
                     Part 2: %s
-                """, dayNumber,
-                DayManager.getDayInstance(dayNumber).partOne(),
-                DayManager.getDayInstance(dayNumber).partTwo()));
+
+                Time elapsed: %02d:%02d.%03d
+                """,
+                dayNumber,
+                partOne,
+                partTwo,
+                minutes, seconds, millis));
     }
 }
