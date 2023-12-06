d18638cf79702f9fb46d9be87f9c5aa793e251b3
Samuel Nitzsche <49833818+saminitz@users.noreply.github.com>
2023-12-06 16:16:15 +0100

    changed day 0 to be a template

--- src/main/java/de/samintiz/adventofcode2023/day00/Day00.java
+++ src/main/java/de/samintiz/adventofcode2023/day00/Day00.java
@@ -1,22 +1,28 @@
 package de.samintiz.adventofcode2023.day00;
 
+import java.util.List;
+
 import de.samintiz.adventofcode2023.day.Day;
+import de.samintiz.adventofcode2023.reader.InputFile;
+import de.samintiz.adventofcode2023.reader.InputReader;
 
 public class Day00 implements Day {
 
+    private List<String> allLines;
+
     @Override
     public void init() {
-        System.out.println("Test: init");
+        allLines = new InputReader(this, InputFile.INPUT).readAllLines();
     }
 
     @Override
     public String partOne() {
-        return "Test: " + this.hashCode() + "partOne";
+        return String.valueOf("Not implemented");
     }
 
     @Override
     public String partTwo() {
-        return "Test: " + this.hashCode() + "partTwo";
+        return String.valueOf("Not implemented!");
     }
 
 }
